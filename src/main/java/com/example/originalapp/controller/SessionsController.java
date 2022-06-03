71d5e582cf62401f1ef307e595642022b4135579
hamuichiro <hamuichiro8616@icloud.com>
2022-06-03 23:43:27 +0900

    #3

---
+++ src/main/java/com/example/originalapp/controller/SessionsController.java
@@ -0,0 +1,15 @@
+package com.example.originalapp.controller;
+
+import org.slf4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SessionsController {
+
+    @GetMapping(path = "/login")
+    public String index() {
+        return "sessions/login";
+    }
+}
