558b662a4d18c21408e877fe8b2ed27c76540a54
hamuichiro <hamuichiro8616@icloud.com>
2022-06-04 20:42:11 +0900

    #3

---
+++ src/main/java/com/example/originalapp/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.originalapp.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.originalapp.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+    User findByUsername(String username);
+}
