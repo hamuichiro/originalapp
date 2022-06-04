4515ee3b5a8abd0ea34e69e6ad6bf424d53af469
hamuichiro <hamuichiro8616@icloud.com>
2022-06-04 18:47:31 +0900

    #3

--- src/main/java/com/example/originalapp/controller/SessionsController.java
+++ src/main/java/com/example/originalapp/controller/SessionsController.java
@@ -4,11 +4,12 @@ import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class SessionsController {
 
-    @GetMapping(path = "/login")
+	@RequestMapping("/login")
     public String index() {
         return "session/login";
     }
