e2efd4bd88138a9511222af76fd397a87d2d2993
hamuichiro <hamuichiro8616@icloud.com>
2022-06-18 21:49:15 +0900

    #3

--- src/main/java/com/example/originalapp/controller/PagesController.java
+++ src/main/java/com/example/originalapp/controller/PagesController.java
@@ -100,10 +100,10 @@ public class PagesController {
         
         //月初めの選択
 
-        driver.findElement(By.xpath("/html/body/p7-app/p7-home/div/p7-topbar/nav[2]/div/div/ul/li[2]/a/gl-switchery/span/small")).click(); //レート一覧の非表示
-        driver.findElement(By.xpath("/html/body/p7-app/p7-home/div/p7-topbar/nav[2]/div/div/ul/li[3]/a/gl-switchery/span/small")).click(); //証拠金状況紹介の非表示
+        driver.findElement(By.xpath("/html/body/p7-app/p7-home/div/p7-topbar/nav[2]/div/div/ul/li[2]/a/gl-switchery/span")).click(); //レート一覧の非表示
+        driver.findElement(By.xpath("/html/body/p7-app/p7-home/div/p7-topbar/nav[2]/div/div/ul/li[3]/a/gl-switchery/span")).click(); //証拠金状況紹介の非表示
         /*driver.findElement(By.xpath("/html/body/p7-app/p7-home/div/p7-topbar/nav[2]/div/div/ul/li[5]/a/gl-switchery/span")).click(); //ポジション一覧の非表示
-        driver.findElement(By.xpath("/html/body/p7-app/p7-home/div/p7-topbar/nav[2]/div/div/ul/li[6]/a/gl-switchery/span/small")).click(); //注文一覧の非表示
+        driver.findElement(By.xpath("/html/body/p7-app/p7-home/div/p7-topbar/nav[2]/div/div/ul/li[6]/a/gl-switchery/span")).click(); //注文一覧の非表示
         
         
        
