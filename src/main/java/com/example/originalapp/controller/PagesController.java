32d1bfd0f1fa59cf86a35ed05928faf88f50f8c9
hamuichiro <hamuichiro8616@icloud.com>
2022-06-19 22:42:40 +0900

    #3

--- src/main/java/com/example/originalapp/controller/PagesController.java
+++ src/main/java/com/example/originalapp/controller/PagesController.java
@@ -102,7 +102,7 @@ public class PagesController {
         driver.findElement(By.xpath("/html/body/p7-app/p7-home/div/p7-topbar/nav[2]/div/div/ul/li[2]/a/gl-switchery")).click(); //レート一覧の非表示
         driver.findElement(By.xpath("/html/body/p7-app/p7-home/div/p7-topbar/nav[2]/div/div/ul/li[3]/a/gl-switchery")).click(); //証拠金状況紹介の非表示
         driver.findElement(By.xpath("/html/body/p7-app/p7-home/div/p7-topbar/nav[2]/div/div/ul/li[5]/a/gl-switchery")).click(); //ポジション一覧の非表示
-        driver.findElement(By.xpath("/html/body/p7-app/p7-home/div/p7-topbar/nav[1]/div/div/ul/li[4]")).click(); //全画面表示
+        
         
         /*WebElement element = driver.findElement(By.xpath("/html/body/p7-app/p7-home/div/p7-topbar/nav[2]/div/div/ul/li[6]/a/gl-switchery"));
         element.click(); //注文一覧の非表示
