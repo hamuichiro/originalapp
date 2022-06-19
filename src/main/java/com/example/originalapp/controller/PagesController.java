3d9eed1d18f9304ee1044fddb028554ecf4f2ba9
hamuichiro <hamuichiro8616@icloud.com>
2022-06-19 19:46:22 +0900

    #3

--- src/main/java/com/example/originalapp/controller/PagesController.java
+++ src/main/java/com/example/originalapp/controller/PagesController.java
@@ -43,8 +43,8 @@ public class PagesController {
     @RequestMapping(path = "/selenium")
     public String selenium() {
 
-    	//String  driver_path = "/app/.chromedriver/bin/chromedriver";
-    	String  driver_path = "./exe/chromedriver.exe";
+    	String  driver_path = "/app/.chromedriver/bin/chromedriver";
+    	//String  driver_path = "./exe/chromedriver.exe";
     	
     	String userId = "1318221";
     	String password = "hamuichi24";
