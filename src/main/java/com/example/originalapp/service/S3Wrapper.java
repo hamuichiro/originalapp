5e9676c7203aa677be67b75f5ba9c2b729850543
hamuichiro <hamuichiro8616@icloud.com>
2022-07-08 19:12:26 +0900

    #14

--- src/main/java/com/example/originalapp/service/S3Wrapper.java
+++ src/main/java/com/example/originalapp/service/S3Wrapper.java
@@ -1,15 +1,18 @@
 package com.example.originalapp.service;
 
 import java.io.File;
+
 import java.io.FileInputStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 
+@Service
 public class S3Wrapper {
 	
     @Autowired
