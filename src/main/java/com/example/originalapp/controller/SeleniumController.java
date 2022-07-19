b3e60f6bfac516938d424382ac3a8ef6d8dff27d
hamuichiro <hamuichiro8616@icloud.com>
2022-07-19 17:42:20 +0900

    #14

--- src/main/java/com/example/originalapp/controller/SeleniumController.java
+++ src/main/java/com/example/originalapp/controller/SeleniumController.java
@@ -221,12 +221,12 @@ public class SeleniumController {
         List<WebElement> tradeHistoryAlllist = driver.findElements(By.className("list-body-row")); //全約定履歴の取得
         
         for(int i = 1; i <= tradeHistoryAlllist.size(); i++) {
-        	
-        	String tradeHistory = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div/div[5]/div[3]/div[1]/div[1]/div/div[5]/div/div[3]/div[2]/div["+i+"]")).getText();
         	if(driver.findElements(By.xpath("/html/body/div[1]/div[1]/div/div[5]/div[3]/div[1]/div[1]/div/div[5]/div/div[3]/div[2]/div["+i+"]")).size() == 0) {
-        	  driver.quit();
-	        return "redirect:/analysistool";
-        	}
+          	  driver.quit();
+  	        return "redirect:/analysistool";
+          	}
+        	String tradeHistory = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div/div[5]/div[3]/div[1]/div[1]/div/div[5]/div/div[3]/div[2]/div["+i+"]")).getText();
+
         /*for(WebElement tradeHistoryList : tradeHistoryAlllist) { //個別の履歴の内容をリストに格納
         	
         	WebElement firstResult = new WebDriverWait(driver, Duration.ofSeconds(100))
