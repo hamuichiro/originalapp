85afcc24ca42abeb29276ff924060cbe6ba54678
hamuichiro <hamuichiro8616@icloud.com>
2022-07-19 16:24:35 +0900

    #14

--- src/main/java/com/example/originalapp/controller/SeleniumController.java
+++ src/main/java/com/example/originalapp/controller/SeleniumController.java
@@ -220,12 +220,10 @@ public class SeleniumController {
 
         List<WebElement> tradeHistoryAlllist = driver.findElements(By.className("list-body-row")); //全約定履歴の取得
         
-        for(int i = 1; driver.findElements(By.xpath("/html/body/div[1]/div[1]/div/div[5]/div[3]/div[1]/div[1]/div/div[5]/div/div[3]/div[2]/div["+i+"]")).size() == 1; i++) {
+        for(int i = 1; i <= tradeHistoryAlllist.size(); i++) {
         	try {
         	
         	String tradeHistory = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div/div[5]/div[3]/div[1]/div[1]/div/div[5]/div/div[3]/div[2]/div["+i+"]")).getText();
-
-        
 
         	ArrayList<String> tradeList = new ArrayList<String>(Arrays.asList(tradeHistory.split("\n")));
 
