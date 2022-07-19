f6f1c8712510ecb1bcb5349cd2b05164315c0491
hamuichiro <hamuichiro8616@icloud.com>
2022-07-19 17:13:19 +0900

    #14

--- src/main/java/com/example/originalapp/controller/SeleniumController.java
+++ src/main/java/com/example/originalapp/controller/SeleniumController.java
@@ -112,8 +112,8 @@ public class SeleniumController {
     }
     
     public ChromeDriver driver() {
-		//String  driver_path = "/app/.chromedriver/bin/chromedriver";
-	    String  driver_path = "./exe/chromedriver.exe";
+		String  driver_path = "/app/.chromedriver/bin/chromedriver";
+	    //String  driver_path = "./exe/chromedriver.exe";
 		
 		ChromeOptions options = new ChromeOptions();
 		
