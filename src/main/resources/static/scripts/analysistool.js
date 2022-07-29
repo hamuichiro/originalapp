47b810b044de39cc565cd48a0c66d3df7f8dc2af
hamuichiro <hamuichiro8616@icloud.com>
2022-07-29 16:48:15 +0900

    #14

--- src/main/resources/static/scripts/analysistool.js
+++ src/main/resources/static/scripts/analysistool.js
@@ -75,7 +75,7 @@ $(function () {
 	$.ajax({
       url: '/screenShot',
       dataType: 'json',
-      data: {filePath: 'filePath'},
+      data: {filePath: filePath},
       type: "GET"
     })
     .done(function (screenShotFile) {
